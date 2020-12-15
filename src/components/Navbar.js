63c50c354ac22b473ee1613a04155a814b2a471f
carledwardfp <official.carlpahuyo@gmail.com>
2020-12-16 02:34:22 +0800

    sleep na

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import './Navbar.css'
 function Navbar() {
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
-    const closeMenu = () => setClick(false);
+    const closeMenu = () => setClick(!click);
     return (
         <nav className="navbar">
             <h1 className="navbar-logo"><span className="spanwhite">CARL</span>EDWARD</h1>
