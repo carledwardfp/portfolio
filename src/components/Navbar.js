414e43eaa6cb4873c074d2d18c0d4d5b279a882d
carledwardfp <official.carlpahuyo@gmail.com>
2020-12-16 01:53:09 +0800

    i can sleep now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MenuItems from './MenuItems'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 
-class Navbar extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            clicked: false
-        }
-        this.handleClick = this.handleClick.bind(this)
-    }
-    handleClick() {
-        this.setState({
-            clicked: !this.state.clicked
-        })
-    }
-
-    render () {
-        return (
-            <nav className="navbar">
-                <h1 className="navbar-logo"><span className="spanwhite">CARL</span>EDWARD</h1>
-                <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                    {MenuItems.map((item, index) => {
-                        return (
-                            <li key={index}>
-                                <Link className={item.class} to={item.url}>
-                                    {item.title}
-                                </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <a href='/contact'><button className='btn'>Contact Me</button></a>
-            </nav>
-        )
-    }
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMenu = () => setClick(false);
+    return (
+        <nav className="navbar">
+            <h1 className="navbar-logo"><span className="spanwhite">CARL</span>EDWARD</h1>
+            <div className="menu-icon" onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                {MenuItems.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <Link className={item.class} to={item.url} onClick={closeMenu}>
+                                {item.title}
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+            <Link to='/contact' onClick={closeMenu}><button className='btn'>Contact Me</button></Link>
+        </nav>
+    )
 }
+
 export default Navbar
