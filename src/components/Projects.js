ed9e0a657d17c5c865cfec24464141fc64ae3082
official-carledwardfp <official.carlpahuyo@gmail.com>
2020-12-29 18:57:10 +0800

    added new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,97 +16,51 @@ const transition = {
 function Projects() {
     return (
         <div className='projects-container'>
-            <div className='projects'>
-                <a href='https://official-carledwardfp.github.io/practice-project-todo/' target='_blank' rel="noreferrer">
-                    <motion.div className='project'
-                        exit='out'
-                        animate='in'
-                        initial='out'
-                        variants={transition}
-                        transition={{duration: .5, stiffness: 1000}}
-                    >
-                        <img src={process.env.PUBLIC_URL+'/images/practice-project-todo.png'} alt='todo'/>
-                        <div className='project-text'>
-                            <h1>Simple Todo App</h1>
-                            <h2>&lt;Practice Project&gt;</h2>
-                        </div>
-                        <p>Made with:</p>
-                        <div className='languages'>
-                            <img src={process.env.PUBLIC_URL+'/images/react.png'}  alt='react'/>
-                            <img src={process.env.PUBLIC_URL+'/images/css.png'}  alt='css'/>
-                            <img src={process.env.PUBLIC_URL+'/images/js.png'}  alt='js'/>
-                        </div>
-                    </motion.div>
-                </a>
+            <div className='projects-container'>
+                <div className='projects'>
+                    <a href='https://official-carledwardfp.github.io/practice-project-todo/' target='_blank' rel="noreferrer">
+                        <motion.div className='project'
+                            exit='out'
+                            animate='in'
+                            initial='out'
+                            variants={transition}
+                            transition={{duration: .5, stiffness: 1000}}
+                        >
+                            <img src={process.env.PUBLIC_URL+'/images/practice-project-todo.png'} alt='todo'/>
+                            <div className='project-text'>
+                                <h1>Simple Todo App</h1>
+                                <h2>&lt;Practice Project&gt;</h2>
+                            </div>
+                            <p>Made with:</p>
+                            <div className='languages'>
+                                <img src={process.env.PUBLIC_URL+'/images/react.png'}  alt='react'/>
+                                <img src={process.env.PUBLIC_URL+'/images/css.png'}  alt='css'/>
+                            </div>
+                        </motion.div>
+                    </a>
 
-                <a href='https://official-carledwardfp.github.io/practice-project-todo/' target='_blank' rel="noreferrer">
-                    <motion.div className='project'
-                        exit='out'
-                        animate='in'
-                        initial='out'
-                        variants={transition}
-                        transition={{duration: .5, stiffness: 1000, delay: 0.25}}
-                    >
-                        <img src={process.env.PUBLIC_URL+'/images/practice-project-todo.png'} alt='todo'/>
-                        <div className='project-text'>
-                            <h1>Simple Todo App</h1>
-                            <h2>&lt;Practice Project&gt;</h2>
-                        </div>
-                        <p>Made with:</p>
-                        <div className='languages'>
-                            <img src={process.env.PUBLIC_URL+'/images/react.png'}  alt='react'/>
-                            <img src={process.env.PUBLIC_URL+'/images/css.png'}  alt='css'/>
-                            <img src={process.env.PUBLIC_URL+'/images/js.png'}  alt='js'/>
-                        </div>
-                    </motion.div>
-                </a>
-
-                <a href='https://official-carledwardfp.github.io/practice-project-todo/' target='_blank' rel="noreferrer">
-                    <motion.div className='project'
-                        exit='out'
-                        animate='in'
-                        initial='out'
-                        variants={transition}
-                        transition={{duration: .5, stiffness: 1000, delay: 0.5}}
-                    >
-                        <img src={process.env.PUBLIC_URL+'/images/practice-project-todo.png'} alt='todo'/>
-                        <div className='project-text'>
-                            <h1>Simple Todo App</h1>
-                            <h2>&lt;Practice Project&gt;</h2>
-                        </div>
-                        <p>Made with:</p>
-                        <div className='languages'>
-                            <img src={process.env.PUBLIC_URL+'/images/react.png'}  alt='react'/>
-                            <img src={process.env.PUBLIC_URL+'/images/css.png'}  alt='css'/>
-                            <img src={process.env.PUBLIC_URL+'/images/js.png'}  alt='js'/>
-                        </div>
-                    </motion.div>
-                </a>
-
-                <a href='https://official-carledwardfp.github.io/practice-project-todo/' target='_blank' rel="noreferrer">
-                    <motion.div className='project'
-                        exit='out'
-                        animate='in'
-                        initial='out'
-                        variants={transition}
-                        transition={{duration: .5, stiffness: 1000, delay: 0.75}}
-                    >
-                        <img src={process.env.PUBLIC_URL+'/images/practice-project-todo.png'} alt='todo'/>
-                        <div className='project-text'>
-                            <h1>Simple Todo App</h1>
-                            <h2>&lt;Practice Project&gt;</h2>
-                        </div>
-                        <p>Made with:</p>
-                        <div className='languages'>
-                            <img src={process.env.PUBLIC_URL+'/images/react.png'}  alt='react'/>
-                            <img src={process.env.PUBLIC_URL+'/images/css.png'}  alt='css'/>
-                            <img src={process.env.PUBLIC_URL+'/images/js.png'}  alt='js'/>
-                        </div>
-                    </motion.div>
-                </a>
-
-                
-
+                    <a href='https://official-carledwardfp.github.io/practice-project-todo/' target='_blank' rel="noreferrer">
+                        <motion.div className='project'
+                            exit='out'
+                            animate='in'
+                            initial='out'
+                            variants={transition}
+                            transition={{duration: .5, stiffness: 1000, delay: 0.25}}
+                        >
+                            <img src={process.env.PUBLIC_URL+'/images/practice-project-stockm.png'} alt='todo'/>
+                            <div className='project-text'>
+                                <h1>BTC/Stock Market</h1>
+                                <h2>&lt;Practice Project&gt;</h2>
+                            </div>
+                            <p>Made with:</p>
+                            <div className='languages'>
+                                <img src={process.env.PUBLIC_URL+'/images/react.png'}  alt='react'/>
+                                <img src={process.env.PUBLIC_URL+'/images/axios.png'}  alt='axios'/>
+                                <img src={process.env.PUBLIC_URL+'/images/chartjs.png'}  alt='chartjs'/>
+                            </div>
+                        </motion.div>
+                    </a>
+                </div>
             </div>
         </div>
     )
